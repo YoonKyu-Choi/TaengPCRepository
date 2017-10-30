cc49e2d34c116c464d97705269b295954d55b192
ChoiTaeeun <dlsdud9857@naver.com>
2017-10-30 16:38:06 +0900

    ㅁㄴㅇ

---
+++ taeng/sql/item.sql
@@ -0,0 +1,5 @@
+create table item(
+	item_name varchar2(20) primary key,
+	item_price number(5) not null,
+	item_stock number(2) not null	
+);
--- taeng/src/main/java/com/kosta/taeng/vo/Item.java
+++ taeng/src/main/java/com/kosta/taeng/vo/Item.java
@@ -1,5 +1,7 @@
 package com.kosta.taeng.vo;
 
-public class Item {
+import java.io.Serializable;
 
+public class Item implements Serializable{
+	
 }
