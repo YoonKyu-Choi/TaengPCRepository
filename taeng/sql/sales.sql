c9bcc4a7aa01654fd12303b74c6b41fa7187b68d
UmTaegyeong <um006500@gmail.com>
2017-10-30 15:27:33 +0900

    table 이미지 넣음!

---
+++ taeng/sql/sales.sql
@@ -0,0 +1,9 @@
+
+drop table sales constraint cascade;
+
+create table sales(
+	sales_date DATE,
+	sales_pccost NUMBER(7),
+	sales_itemprice NUMBER(5),
+	item_name VARCHAR2(20) CONSTRAINT fk_item_name REFERENCES item 
+)
