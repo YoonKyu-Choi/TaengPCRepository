64dcab16e91e69c7b35b51bfd21af0f1157e430e
YoonKyuChoi <dbsrb05111@naver.com>
2017-11-01 10:36:55 +0900

    gg

--- taeng/sql/item.sql
+++ taeng/sql/item.sql
@@ -1,9 +1,9 @@
-drop table item
+drop table item;
 create table item(
 	item_name varchar2(20) primary key,
 	item_price number(5) not null,
 	item_stock number(3) not null,
 	item_image varchar2(100)
 );
-select * from item
-delete from item
+select * from item;
+delete from item;
