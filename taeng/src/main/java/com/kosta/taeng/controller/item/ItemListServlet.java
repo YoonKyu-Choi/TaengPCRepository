f93f5ee45177e3347370c3f2195afe8027f2eea6
LeeHyoJu <mint0490@naver.com>
2017-11-03 11:58:45 +0900

    커커커ㅣㅁㅅ

--- taeng/src/main/java/com/kosta/taeng/controller/item/ItemListServlet.java
+++ taeng/src/main/java/com/kosta/taeng/controller/item/ItemListServlet.java
@@ -21,7 +21,7 @@ public class ItemListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		ItemService service = ItemServiceImpl.getInstance();
 		
